f1d461f5039ffbf970c2711112ede9e94accab2b
Ritam Mahato <ritammahato15@gmail.com>
2022-01-12 12:27:59 +0530

    Demo Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { EmpListComponent } from './emp-list/emp-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    EmpListComponent
   ],
   imports: [
     BrowserModule,
